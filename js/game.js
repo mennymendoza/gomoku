14e493646eeafdab140e1a43eb2830bdc7523c05
mennymendoza <Juanmendoza97@mail.fresnostate.edu>
2021-10-23 10:57:00 -0700

    declared classes

--- js/game.js
+++ js/game.js
@@ -1,1 +1,26 @@
+// All classes used in the game
 
+class Game {
+    constructor(boardSize) {
+        this.numberOfTurns = 0;
+        this.playerTurn = 1;
+        this.size = boardSize;
+        this.board = []
+    }
+    // Updates the board when the player clicks on a cell.
+    updateTable(cellId)
+    // Checks to see if the current player has won.
+    playerWins()
+    // Given a cellId, returns an array containing x and y coordinates.
+    getCoords()
+}
+
+class Tile {
+    constructor(cellId, x, y) {
+        this.id = cellId;
+        this.xIndex = x;
+        this.yIndex = y;
+        this.empty = true;
+        this.playerOwned = null;
+    }
+}
