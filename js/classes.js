d8989adf5036b9a55855250c1671b0d7e2fc3d5b
mennymendoza <Juanmendoza97@mail.fresnostate.edu>
2021-10-27 20:08:17 -0700

    fixed function name

--- js/classes.js
+++ js/classes.js
@@ -7,6 +7,7 @@ class Game {
         this.numberOfTurns = 0;
         this.playerTurn = 1;
         this.size = boardSize;
+        this.currentPlayer;
         this.boardArray = new Array();
 
         // Constucts board array and creates HTML table to represent board.
@@ -30,18 +31,18 @@ class Game {
         // index of cell that was clicked (for accessing the array)
         let cellIndex = parseInt(cellId.substring(5));
         
-        // id of player who clicked them (0 or 1)
-        let currentPlayer = this.playerTurn % 2;
+        // Set global variable to id of current player (0 or 1)
+        this.currentPlayer = this.playerTurn % 2;
 
         if (this.boardArray[cellIndex].empty) {
             // if the board is empty
             this.decorateCell(cellId);
             this.boardArray[cellIndex].empty = false;
-            this.boardArray[cellIndex].playerOwned = currentPlayer;
+            this.boardArray[cellIndex].playerOwned = this.currentPlayer;
             
             // checks if player won
             if (this.playerWins(cellIndex)) {
-                alert("Player " + currentPlayer.toString() + " wins!");
+                alert("Player " + this.currentPlayer.toString() + " wins!");
                 return;
             }
 
@@ -58,11 +59,8 @@ class Game {
         // Gets HTML element of the cell to decorate
         let selectedCell = document.getElementById(cellId);
 
-        // id of player who clicked them (0 or 1)
-        let currentPlayer = this.playerTurn % 2;
-
         // checks which player's turn it is and decorates accordingly
-        if (currentPlayer) {
+        if (this.currentPlayer) {
             selectedCell.style.backgroundColor = "#000";
         }
         else {
@@ -72,11 +70,22 @@ class Game {
 
     // Checks to see if the current player has won.
     playerWins(cellIndex) {
-        return false;
+        let wins = true;
+        // Checks for bottom right diagonal wins
+        for (let i = 0; i < 5; i++) {
+            let nextIndex = this.getIndex(cellIndex + i, cellIndex + j)
+            if (nextIndex >= this.size || nextIndex < 0) {
+                break;
+            }
+            if (this.boardArray[nextIndex] != this.currentPlayer) {
+                wins = false;
+            }
+        }
+        return wins;
     }
     // Given a cellId, returns an array containing x and y coordinates.
-    getCoords(cellIndex) {
-        // code goes here
+    getIndex(x, y) {
+        return this.size * y + x;
     }
 }
 
