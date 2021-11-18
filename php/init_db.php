d8ce9ac420706f5c164c612e3c13140e8098290e
Juan Mendoza <Juanmendoza97@mail.fresnostate.edu>
2021-11-18 15:06:12 -0800

    created a drop db script

--- php/init_db.php
+++ php/init_db.php
@@ -1,17 +1,17 @@
 <?php
 
 # Connects to mysql database
-$conn = mysqli("localhost", "juan", "password");
+$conn = new mysqli("localhost", "juan", "password");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error . "<br />");
 }
 else {
-    echo "Connected successfully.";
+    echo "Connected successfully.<br />";
 }
 
 # Runs a query to create a database
-$query_success = $conn->query("CREATE DATABASE games");
+$query_success = $conn->query("CREATE DATABASE gomoku");
 
 if ($query_success) {
     echo "Database created successfully.<br />";
