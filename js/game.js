e20781c705513362643cf17fecc92615ca3a83db
IPrz17 <perezisrael1798@gmail.com>
2021-11-24 20:53:35 -0800

    Options button

--- js/game.js
+++ js/game.js
@@ -7,3 +7,10 @@ const restartGame = () => {
     let consoleBox = document.querySelector("#console-alert");
     consoleBox.innerHTML = "GOMOKU GAME";
 }
+
+function openButton() {
+    document.querySelector('#options-menu').style.display = "block"
+}
+function closeButton() {
+    document.querySelector('#options-menu').style.display = "none"
+}
